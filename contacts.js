50df93ae4461a7f63e143cf2aa4018e03984052d
AnatoliyDanets <danetsxp@gmail.com>
2021-12-09 22:17:53 +0200

    node hw-1

---
+++ contacts.js
@@ -0,0 +1,68 @@
+const fs = require("fs").promises;
+const path = require("path");
+const { nanoid } = require("nanoid");
+const contactsPath = path.join(__dirname, "/db/contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  const res = JSON.parse(data);
+  return res;
+}
+
+async function getContactById(contactId) {
+  const data = await listContacts();
+  try {
+    const result = data.filter(({ id }) => +id === +contactId);
+    console.table(result);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function removeContact(contactId) {
+  const data = await listContacts();
+  try {
+    const result = data.filter(({ id }) => id !== contactId);
+    await fs.writeFile(contactsPath, JSON.stringify(result, null, 2));
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
+async function addContact(name, email, phone) {
+  const data = await listContacts();
+  try {
+    const newContact = { id: nanoid(), name, email, phone };
+    data.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
+    return newContact;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function updateContact(data) {
+  await fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
+}
+async function updateById({ id, name, email, phone }) {
+  const data = await listContacts();
+  try {
+    const idx = data.findIndex((item) => item.id === id);
+    if (idx === -1) {
+      return null;
+    }
+    data[idx] = { id, name, email, phone };
+    await updateContact(data);
+    return data[idx];
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateById,
+};
---
+++ index.js
@@ -0,0 +1,49 @@
+const contacts = require("./contacts");
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <list>", "choose action")
+  .option("-i, --id <get>", "user id")
+  .option("-i, --id <remove>", "user id")
+  .option("-n, --name <add>", "user name")
+  .option("-e, --email <add>", "user email")
+  .option("-p, --phone <add>", "user phone")
+  .option("-n, --name <update>", "user name")
+  .option("-e, --email <update>", "user email")
+  .option("-p, --phone <update>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+// TODO: рефакторить
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      contacts.listContacts().then((data) => console.table(data));
+      break;
+
+    case "get":
+      contacts.getContactById(id).then((data) => console.table(data));
+      break;
+
+    case "add":
+      contacts
+        .addContact(name, email, phone)
+        .then((data) => console.table(data));
+      break;
+
+    case "remove":
+      contacts.removeContact(id).then((data) => console.table(data));
+      break;
+    case "update":
+      contacts
+        .updateById({ id, name, email, phone })
+        .then((data) => console.table(data));
+      break;
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
